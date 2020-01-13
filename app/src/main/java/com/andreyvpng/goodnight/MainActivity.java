37513c9994d61cf3804e4dd7c6f3cc230b84b4cb
andrey <andreywvarfolomeev@protonmail.com>
2020-01-13 17:42:40 +1000

    fixed

--- app/src/main/java/com/andreyvpng/goodnight/MainActivity.java
+++ app/src/main/java/com/andreyvpng/goodnight/MainActivity.java
@@ -6,31 +6,38 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
-
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private OnClickListener playButton = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText text = findViewById(R.id.activity_main__input_timer);
+            String str = text.getText().toString();
+            RadioGroup colors_group = findViewById(R.id.activity_main__group_colors);
+            RadioButton button_color = findViewById(colors_group.getCheckedRadioButtonId());
+
+            Intent intent = new Intent(getApplicationContext(), NightLightActivity.class);
+            intent.putExtra("TIMER", str);
+            intent.putExtra("COLOR", button_color.getText().toString());
+            startActivity(intent);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Objects.requireNonNull(getSupportActionBar()).hide();
         FloatingActionButton play_button = findViewById(R.id.activity_main__play_button);
-        play_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText text = findViewById(R.id.activity_main__input_timer);
-                String str = text.getText().toString();
-
-
-                Intent intent = new Intent(getApplicationContext(), NightLightActivity.class);
-                intent.putExtra("TIMER", str);
-                startActivity(intent);
-            }
-        });
+        play_button.setOnClickListener(playButton);
     }
+
+
 }
--- app/src/main/java/com/andreyvpng/goodnight/NightLightActivity.java
+++ app/src/main/java/com/andreyvpng/goodnight/NightLightActivity.java
@@ -3,26 +3,45 @@ package com.andreyvpng.goodnight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Field;
 import java.util.Objects;
 
 public class NightLightActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_night_light);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    public int getColorByName(String name) {
+        int colorId = 0;
 
-        String timer_str = getIntent().getStringExtra("TIMER");
-        int timer_time = 0;
-        if (!Objects.equals(timer_str, ""))
-            timer_time = Integer.parseInt(timer_str);;
+        try {
+            Class res = R.color.class;
+            Field field = res.getField(name);
+            colorId = field.getInt(null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return colorId;
+    }
+
+    void setBackground(Intent intent) {
+        String color = Objects.requireNonNull(intent.getStringExtra("COLOR")).toLowerCase();
+        getWindow().getDecorView().setBackgroundColor(getResources().getColor(getColorByName(color)));
+    }
+
+    void setTimer(Intent intent) {
+        String timer_str = intent.getStringExtra("TIMER");
+        int timer_time;
+        if (timer_str != null && !Objects.equals(timer_str, ""))
+            timer_time = Integer.parseInt(timer_str);
+        else
+            timer_time = 0;
+
         new CountDownTimer(1000 * 60 * timer_time, 1000) {
             TextView textView = findViewById(R.id.activity_night_light__textView);
             @SuppressLint("DefaultLocale")
@@ -42,5 +61,16 @@ public class NightLightActivity extends AppCompatActivity {
                 finish();
             }
         }.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night_light);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        Intent intent = getIntent();
+        setBackground(intent);
+        setTimer(intent);
     }
 }
